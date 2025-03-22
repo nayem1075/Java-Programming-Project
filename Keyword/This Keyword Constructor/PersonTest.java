8381324806a2f839207487884bd3a7baf93e4f69
Nayem Uddin <nayemuddin2006@gmail.com>
2025-03-22 19:20:22 +0600

    Add files via upload

---
+++ Keyword/This Keyword Constructor/PersonTest.java
@@ -0,0 +1,16 @@
+
+package ThisKeywordConstructor;
+
+
+public class PersonTest {
+    
+    public static void main(String[] args) {
+        
+        Person p1 = new Person("Abir",22);
+        p1.display();
+        
+        Person p2 = new Person("Nayem",20,"black");
+        p2.display();
+    }
+    
+}

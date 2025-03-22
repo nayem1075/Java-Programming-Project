8d1fd48e3a6f201e5eac0c8fe150c4484f71be93
Nayem Uddin <nayemuddin2006@gmail.com>
2025-03-22 13:49:31 +0600

    Add files via upload

---
+++ Java Basic/Escapesequence.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.java1;
+
+
+public class Escapesequence {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Nayem Uddin \n01830311622 \nBsc in CSE at University Of Science And Techonology");
+        System.out.println(1);//It's not string so no double quote
+        System.out.println("1");//Here it's not string but use double quote Escape sequence
+        System.out.println("1 \t 2");//Display 2 after tab
+        System.out.println("3 \t 4");//Display 4 after tab
+        System.out.println("\"Java Programming\"");//For print double quote
+        System.out.println(" \bUndergraduate");//For print backspace
+        System.out.println("\rUndergraduate");//For print After one line and which called carriage return
+        System.out.println("\\Undergraduate");//For print backslash
+    }
+    
+}

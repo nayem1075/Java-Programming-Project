c36df4a86485def5bee8b9431ce9c46a47ec3bdc
Nayem Uddin <nayemuddin2006@gmail.com>
2025-03-22 19:21:07 +0600

    Add files via upload

---
+++ Keyword/This Keyword Instance Variable/PersonTest.java
@@ -0,0 +1,12 @@
+
+package ThisKeywordInstanceVariable;
+
+
+public class PersonTest {
+    
+    public static void main(String[] args) {
+        
+        Person ob = new Person("Nayem",20);
+        ob.display();
+    }
+}
